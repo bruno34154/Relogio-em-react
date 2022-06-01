4253c2f9839725037b84304de429804c59c8b65c
bruno34154 <www.brunomesenga@gmail.com>
2022-06-01 18:41:58 -0300

    first commit

---
+++ src/components/clock/index.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+
+export default function Clock() {
+  const date = new Date();
+  const [seconds, setSeconds] = useState(date.getSeconds());
+  const [minutes, setMinutes] = useState(date.getMinutes());
+  const [hours, setHours] = useState(date.getHours());
+  let time = `${hours}:${minutes}:${seconds}`;
+  useEffect(() => {
+    const interval = setInterval(clocktime, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  function clocktime() {
+    setSeconds(seconds + 1);
+
+    if (seconds === 59) {
+      setSeconds(0);
+
+      setMinutes(minutes + 1);
+    }
+    if (minutes === 59) {
+      setMinutes(0);
+
+      setHours(hours + 1);
+    }
+    if (hours === 24) {
+      setHours(0);
+    }
+  }
+  if (seconds < 10 && minutes < 10 && hours < 10) {
+    time = `0${hours}:0${minutes}:0${seconds}`;
+  } else if (seconds < 10 && minutes < 10) {
+    time = `${hours}:0${minutes}:0${seconds}`;
+  } else if (minutes < 10 && hours < 10) {
+    time = `0${hours}:0${minutes}:${seconds}`;
+  } else if (seconds < 10 && hours < 10) {
+    time = `0${hours}:${minutes}:0${seconds}`;
+  } else if (seconds < 10) {
+    time = `${hours}:${minutes}:0${seconds}`;
+  } else if (minutes < 10) {
+    time = `${hours}:0${minutes}:${seconds}`;
+  } else if (hours < 10) {
+    time = `0${hours}:${minutes}:${seconds}`;
+  }
+
+  return (
+    <div className="container-clock">
+      <input className="input-clock" value={time} readOnly></input>
+    </div>
+  );
+}
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export default function Navbar() {
+  return (
+    <div className="container-nav">
+      <h1>horario local brasilia</h1>
+    </div>
+  );
+}
